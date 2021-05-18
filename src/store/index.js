6ad6e8c6700d4c6269fec356832b5dd6d7907f73
Nikhil Gautam <nikhilgautam992@gmail.com>
2021-05-18 13:02:59 +0530

    final changes

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,8 @@
 import { applyMiddleware, createStore } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "../reducers";
-import { composeWithDevTools } from "redux-devtools-extension";
+// import { composeWithDevTools } from "redux-devtools-extension";
 
-const store = createStore(
-    rootReducer,
-    composeWithDevTools(applyMiddleware(thunk))
-);
+const store = createStore(rootReducer);
 
 export default store;
